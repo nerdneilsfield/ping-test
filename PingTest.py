4338ed8d3f71282bce12a3390a9ca1aa22cd53be
dengqi <dengqi@dengqideiMac.local>
2018-02-28 23:51:23 +0800

    add name

--- PingTest.py
+++ PingTest.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request, redirect
 from utils import DoPing
 from tinydb import TinyDB, Query
-
+from socket import gethostbyname
 
 app = Flask(__name__)
 
@@ -14,14 +14,19 @@ def index():
     ips = db.all()
     db.close()
     if len(ips) != 0:
-        targets = [i['ip'] for i in ips]
+        targets = [i['target'] for i in ips]
         results = DoPing(targets)
         addrs = []
         rtts =[]
+        names = []
+        print(ips)
         for addr, rtt in results.items():
             addrs.append(addr)
             rtts.append(rtt)
-        
+        for addr in addrs:
+            for i in ips:
+                if  gethostbyname(i["target"]) == addr:
+                    names.append(i["name"])
         # if len(addrs) == 1:
         #     addrs = '["'+addrs[0]+'"]'
         # else:
@@ -30,17 +35,20 @@ def index():
         #     addrs += '"'+temp[-1]+'"]'
         print(addrs)
         print(rtts)
-        return render_template("index.html", addrs="#".join(addrs), rtts=str(rtts))
+        return render_template("index.html", addrs="#".join(names), rtts=str(rtts))
     else:
         return render_template("index.html", addrs='没有', rtts=[12])
 
 @app.route('/add', methods=['POST'])
 def postadd():
     ip = request.form['addip']
+    name = request.form['name']
     db = TinyDB('ips.json')
-    db.insert({'ip':ip})
-    db.close()
-    print(db)
+    Target = Query()
+    sd = db.search(Target.name  == name)
+    if len(sd) == 0:
+        db.insert({'target':ip, 'name':name})
+        db.close()
     return redirect('/')
 
 if __name__ == "__main__":
